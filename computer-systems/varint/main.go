ee848769b8eb9258c7a0bde0ec91a1b411032ba8
Jack Fowler <jackmihalik@icloud.com>
2024-06-27 18:26:13 +0900

    Improved protobuf encode/decode logic

--- computer-systems/varint/main.go
+++ computer-systems/varint/main.go
@@ -1,7 +1,6 @@
 package main
 
 import (
-	"encoding/hex"
 	"fmt"
 	"os"
 	"slices"
@@ -10,63 +9,47 @@ import (
 )
 
 func main() {
-	data, err := os.ReadFile("150.uint64")
+	files := []string{"150.uint64"}
+	// files := []string{"1.uint64", "150.uint64", "maxint.uint64"}
+	for _, fileName := range files {
+		data, err := os.ReadFile(fileName)
 
-	if err != nil {
-		panic(err)
+		if err != nil {
+			panic(err)
+		}
+		val := unpack(data)
+
+		fmt.Printf("Protobuf encoded varint %d -> % x decoded -> %d \n\n", val, encode(val), decode(encode(val)))
+
 	}
-
-	val := unpack(data)
-
-	fmt.Printf("result %s ", hex.Dump(encode(val)))
-	fmt.Println(decode(encode(val)))
 	return
 }
 
-func decode(bytes []byte) uint64 {
-	var s []string
-	for _, byte := range bytes {
-		eightBits := fmt.Sprintf("%08b", uint64(byte))
-		s = append(s, eightBits[1:])
+func encode(n uint64) []byte {
+
+	var out []byte
+
+	for n > 0 {
+		part := n & 0x7f
+		n >>= 7
+		if n > 0 {
+			part |= 0x80
+		}
+		out = append(out, byte(part))
 	}
 
-	slices.Reverse(s)
-	joined := strings.Join(s, "")
-	val, _ := strconv.ParseUint(joined, 2, 64)
-	return val
+	return out
 }
 
-func encode(n uint64) []byte {
-	if n < 128 {
-		b := strconv.FormatUint(n, 2)
-		b = fmt.Sprintf("%s%s", strings.Repeat("0", (8-len(b))%8), b)
-		v, _ := strconv.ParseUint(b, 2, 8)
-		var bytes []byte
-		return append(bytes, byte(v))
+func decode(varn []byte) uint64 {
+	slices.Reverse(varn)
+
+	var n uint64
+	for _, byte := range varn {
+		n <<= 7
+		n |= uint64((byte & 0x7f))
 	}
-
-	b := strconv.FormatUint(n, 2)
-	var s string
-	for i := 0; i < len(b); i += 7 {
-		if (len(b)-i)-7 > 0 {
-			split := b[(len(b)-i)-7 : len(b)-i]
-			s = fmt.Sprintf("1%s %s", split, s)
-		} else {
-			s = fmt.Sprintf("0%s%s %s", strings.Repeat("0", 7-len(b)%i), b[0:len(b)%i], s)
-		}
-	}
-
-	s = strings.TrimSpace(s)
-	bytes := strings.Split(s, " ")
-	slices.Reverse(bytes)
-
-	var dump []byte
-	for _, s := range bytes {
-		v, _ := strconv.ParseUint(s, 2, 8)
-		dump = append(dump, byte(v))
-	}
-
-	return dump
+	return n
 }
 
 func unpack(bytes []byte) uint64 {
@@ -78,5 +61,6 @@ func unpack(bytes []byte) uint64 {
 		item, _ := strconv.ParseUint(arr[i], 16, 64)
 		total += item
 	}
+
 	return total
 }
