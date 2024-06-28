60d120d7d628077ec490fd53ed20fa2f3a7fcc11
Jack Fowler <jackmihalik@icloud.com>
2024-06-28 11:28:03 +0900

    css converter advanced

--- computer-systems/bits-and-bytes/color-convert/main.go
+++ computer-systems/bits-and-bytes/color-convert/main.go
@@ -8,9 +8,44 @@ import (
 	"strings"
 )
 
-// convert from hexidecimal to rgb
+func main() {
+	scanner := bufio.NewScanner(os.Stdin)
+	for scanner.Scan() {
+		line := scanner.Text()
+		before, after, found := strings.Cut(line, ": #")
 
-// take std out as in put
+		if found {
+			var nums []string
+
+			step := 0
+			if (len(after)-1)%8 == 0 || (len(after)-1)%6 == 0 {
+				step = 1
+			}
+			for i := 0; i < len(after)-1; i++ {
+				sum := charToN(string(after[i]))*16 +
+					charToN(string(after[i+step]))
+				nums = append(nums, strconv.Itoa(sum))
+				i += step
+			}
+
+			if (len(after)-1)%4 == 0 {
+				num1, _ := strconv.Atoi(nums[2])
+				num2, _ := strconv.Atoi(nums[3])
+				fmt.Printf("%s: rgba(%s / %.5f);\n", before, strings.Join(nums[0:3], " "), float64(num2)/float64(num1))
+			} else {
+				fmt.Printf("%s: rgb(%s);\n", before, strings.Join(nums, " "))
+			}
+
+		} else {
+			fmt.Println(line)
+		}
+	}
+
+	if err := scanner.Err(); err != nil {
+		fmt.Fprintln(os.Stderr, "reading standard input:", err)
+	}
+}
+
 func charToN(ch string) int {
 	if i, err := strconv.Atoi(ch); err == nil {
 		return i
@@ -27,31 +62,4 @@ func charToN(ch string) int {
 	}
 
 	return hexMap[ch]
-}
-
-func main() {
-	scanner := bufio.NewScanner(os.Stdin)
-
-	for scanner.Scan() {
-		line := scanner.Text()
-		before, after, found := strings.Cut(line, "#")
-
-		if found {
-			var nums []string
-			for i := 0; i < len(after)-1; i += 2 {
-				sum := charToN(string(after[i]))*16 +
-					charToN(string(after[i+1]))
-
-				nums = append(nums, strconv.Itoa(sum))
-			}
-
-			fmt.Printf("%s: rgb(%s);\n", before[0:len(before)-2], strings.Join(nums, " "))
-		} else {
-			fmt.Println(line)
-		}
-	}
-
-	if err := scanner.Err(); err != nil {
-		fmt.Fprintln(os.Stderr, "reading standard input:", err)
-	}
 }
