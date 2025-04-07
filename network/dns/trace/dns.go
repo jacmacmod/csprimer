3cae1d9393a6859a89ca7bcd1c1884c533002cfb
Jack Fowler <jackmihalik@icloud.com>
2025-04-08 00:08:21 +0900

    refactor dns client

---
+++ network/dns/trace/dns.go
@@ -0,0 +1,208 @@
+package main
+
+import (
+	"encoding/binary"
+	"errors"
+	"log"
+	"syscall"
+)
+
+type RType int
+type RClass int
+type Opcode int
+type Rcode int
+type Flag bool
+type QR int
+
+type DNSMessage struct {
+	Header      Header
+	Questions   []Question
+	Answers     []ResourceRecord
+	Authorities []ResourceRecord
+	Additional  []ResourceRecord
+	Data        []byte
+}
+
+type Header struct {
+	ID                    int
+	QR                    QR
+	OpCode                Opcode
+	AuthoritativeAnswer   Flag
+	Truncation            Flag
+	RecursionDesired      Flag
+	RecursionAvailable    Flag
+	Z                     int
+	RCode                 Rcode
+	QuestionCount         int
+	AnswerCount           int
+	NameServerCount       int
+	AdditionalRecordCount int
+}
+
+type Question struct {
+	Name   string
+	Qtype  RType
+	Qclass RClass
+}
+
+type ResourceRecord struct {
+	Name       string
+	Type       RType
+	Class      RClass
+	TTL        int
+	DataLength int
+	RData      string
+}
+
+const (
+	dnsPort    = 53
+	bufferSize = 4096
+
+	A     RType = 1
+	NS    RType = 2
+	CNAME RType = 5  // canonical name for an alias
+	SOA   RType = 6  // start of zone ability
+	WKS   RType = 11 // well known service descriptor
+	PTR   RType = 12
+	HINFO RType = 13
+	MINFO RType = 14
+	MX    RType = 15
+	TXT   RType = 16
+	AAAA  RType = 28
+	OPT   RType = 41
+
+	IN         RClass = 1
+	CS         RClass = 2
+	CH         RClass = 3
+	HS         RClass = 4
+	OPTRRClass RClass = bufferSize
+
+	Query  Opcode = 0
+	IQuery Opcode = 1
+	Status Opcode = 2
+
+	NoError        Rcode = 0
+	FormatError    Rcode = 1
+	ServerError    Rcode = 2
+	NameError      Rcode = 3
+	NotImplemented Rcode = 4
+	Refused        Rcode = 5
+
+	QRQuery    QR = 0
+	QRResponse QR = 1
+)
+
+var (
+	googleHost = [4]byte{8, 8, 8, 8}
+
+	TypesMap = map[RType]string{
+		A:     "A",
+		NS:    "NS",
+		CNAME: "CNAME",
+		SOA:   "SOA",
+		WKS:   "WKS",
+		PTR:   "PTR",
+		HINFO: "HINFO",
+		MINFO: "MINFO",
+		MX:    "MX",
+		TXT:   "TXT",
+		AAAA:  "AAAA",
+	}
+
+	ClassMap = map[RClass]string{
+		IN: "IN",
+		CS: "CS",
+		CH: "CH",
+		HS: "HS",
+	}
+
+	opcodeMap = map[Opcode]string{
+		Query:  "QUERY",
+		IQuery: "IQUERY",
+		Status: "STATUS",
+	}
+
+	rcodeMap = map[Rcode]string{
+		NoError:        "NOERROR",
+		FormatError:    "FORMATERROR",
+		ServerError:    "SERVERERROR",
+		NameError:      "NAMERERROR",
+		NotImplemented: "NOTIMPLEMENTED",
+		Refused:        "REFUSED",
+	}
+)
+
+type DNSClient struct {
+	address     syscall.SockaddrInet4
+	fd          int
+	fromAddress syscall.SockaddrInet4
+	data        []byte
+}
+
+func newDNSClient(host [4]byte) DNSClient {
+	return DNSClient{
+		address: syscall.SockaddrInet4{
+			Port: dnsPort,
+			Addr: host,
+		},
+	}
+}
+
+func (d *DNSClient) Connect() {
+	fd, err := syscall.Socket(syscall.AF_INET, syscall.SOCK_DGRAM, syscall.IPPROTO_UDP)
+	if err != nil {
+		log.Fatalf("Error creating socket %v\n", err)
+	}
+	d.fd = fd
+}
+
+func (d *DNSClient) Close() {
+	syscall.Close(d.fd)
+}
+
+func (d *DNSClient) Query(q DNSMessage) (DNSMessage, int, error) {
+	if err := syscall.Sendto(d.fd, q.Data, 0, &d.address); err != nil {
+		return DNSMessage{}, 0, err
+	}
+
+	p, bytesRead := make([]byte, bufferSize), 0
+
+	for {
+		n, from, err := syscall.Recvfrom(d.fd, p, 0)
+		bytesRead += n
+		if err != nil {
+			return DNSMessage{}, bytesRead, err
+		}
+
+		s, ok := from.(*syscall.SockaddrInet4)
+		if !ok {
+			return DNSMessage{}, bytesRead, errors.New("unable ro parse address")
+		}
+
+		if s.Addr != d.address.Addr {
+			continue // ignore dns data from other servers
+		}
+
+		if int(binary.BigEndian.Uint16(p[0:2])) != q.Header.ID {
+			return DNSMessage{}, bytesRead, errors.New("X ID does not match")
+		}
+
+		if n < bufferSize {
+			break
+		}
+	}
+
+	validateResponse(p)
+	res, _ := parseResponse(p)
+	return res, bytesRead, nil
+}
+
+func validateResponse(p []byte) {
+	if (p[2]&0x80)>>7 != 1 {
+		log.Fatalln("QR must be 1")
+	}
+
+	if (p[3]&0x70)>>4 != 0 {
+		log.Fatalln("unexpected z value", (p[3]&0x70)>>4)
+	}
+}
---
+++ network/dns/trace/main.go
@@ -0,0 +1,434 @@
+package main
+
+import (
+	"bytes"
+	"encoding/binary"
+	"flag"
+	"fmt"
+	"log"
+	"math/rand"
+	"net/netip"
+	"os"
+	"slices"
+	"strings"
+	"syscall"
+	"time"
+	"unicode/utf8"
+)
+
+type qtypeAndClass struct {
+	Qtype  uint16
+	Qclass uint16
+}
+
+func main() {
+	if len(os.Args) <= 1 {
+		log.Fatalln("no url or ip specified")
+	}
+
+	var ipAddress = flag.String("x", "", "ip address for reverse lookup")
+	flag.Parse()
+
+	url, rtype := os.Args[1], A
+	if len(os.Args) > 2 {
+		// return error more go like
+		rtype = typeStringToRtype(os.Args[2])
+	}
+
+	if *ipAddress != "" {
+		url = *ipAddress
+		if _, err := netip.ParseAddr(url); err == nil {
+			rtype = PTR
+		} else {
+			log.Fatalln("invalid ip address")
+		}
+	}
+
+	DNSQuery(prepareQuery(url, rtype))
+}
+
+func DNSQuery(q DNSMessage) DNSMessage {
+	dnsClient := newDNSClient(googleHost)
+	dnsClient.Connect()
+	defer dnsClient.Close()
+
+	start := time.Now() // bytes received
+	res, n, err := dnsClient.Query(q)
+	if err != nil {
+		log.Fatalf("DNS Query err: %v", err)
+	}
+
+	printResponse(res, start, dnsClient.address, n)
+
+	return res
+}
+
+func prepareQuery(url string, rrtype RType) DNSMessage {
+	var h Header
+	xid, flags := rand.Intn(65535), 0x0100
+	var data struct {
+		XID        uint16
+		Flags      uint16
+		Questions  uint16
+		Answers    uint16
+		Authority  uint16
+		Additional uint16
+	}
+	buf := new(bytes.Buffer)
+	data.XID, data.Flags, data.Questions, data.Additional = uint16(xid), uint16(flags), 1, 1
+	h.ID, h.QR, h.RecursionDesired, h.QuestionCount, h.AdditionalRecordCount = int(data.XID), QRQuery, Flag(true), 1, 1
+	if err := binary.Write(buf, binary.BigEndian, data); err != nil {
+		fmt.Println("binary.Write failed:", err)
+	}
+	b := buf.Bytes()
+
+	octets := strings.Split(url, ".")
+	if rrtype == PTR {
+		slices.Reverse(octets)
+	}
+
+	qname := []byte{}
+	for _, o := range octets {
+		qname = append(qname, uint8(len(o)))
+		qname = append(qname, []byte(o)...)
+	}
+
+	if rrtype == PTR {
+		qname = append(qname, uint8(7))
+		qname = append(qname, []byte("in-addr")...)
+		qname = append(qname, uint8(4))
+		qname = append(qname, []byte("arpa")...)
+	}
+	qname = append(qname, uint8(0))
+
+	b = append(b, qname...)
+
+	buf = new(bytes.Buffer)
+	qtypeAndClassdataStruct := qtypeAndClass{Qtype: uint16(rrtype), Qclass: uint16(IN)}
+	if err := binary.Write(buf, binary.BigEndian, qtypeAndClassdataStruct); err != nil {
+		fmt.Println("binary.Write failed:", err)
+	}
+	qtypeAndClass := buf.Bytes()
+	b = append(b, qtypeAndClass...)
+
+	var OPTRR struct {
+		Name  uint8
+		Type  uint16
+		Class uint16
+		TTL   uint32
+		Rdlen uint16
+	}
+
+	buf = new(bytes.Buffer)
+	OPTRR.Name, OPTRR.Type, OPTRR.Class, OPTRR.TTL, OPTRR.Rdlen = 0, uint16(OPT), uint16(bufferSize), 0, 0
+	if err := binary.Write(buf, binary.BigEndian, OPTRR); err != nil {
+		fmt.Println("binary.Write failed:", err)
+	}
+
+	optrr := buf.Bytes()
+	b = append(b, optrr...)
+
+	return DNSMessage{
+		Header: h,
+		Questions: []Question{
+			{Qtype: rrtype, Qclass: IN, Name: url},
+		},
+		Additional: []ResourceRecord{
+			{Name: "0", Type: OPT, Class: OPTRRClass},
+		},
+		Data: b,
+	}
+}
+
+func readName(p []byte, offset int) (string, int) {
+	labels, i, nextIdx, offsets := []string{}, offset, 0, make(map[int]struct{})
+
+	for {
+		b := p[i]
+		if b&0xC0 > 0 {
+			if nextIdx == 0 {
+				nextIdx = i + 2
+			}
+			i = int(p[i+1])
+			if _, exists := offsets[i]; exists {
+				break
+			}
+			offsets[i] = struct{}{}
+		} else if b == 0x00 { // at the end
+			if nextIdx == 0 {
+				nextIdx = i + 1
+			}
+			break
+		} else {
+			ll := int(b)
+			labels = append(labels, string(p[i+1:i+1+ll]))
+			i += ll + 1
+		}
+	}
+
+	return strings.Join(labels, ".") + ".", nextIdx
+}
+
+func toFlag(b byte) Flag {
+	if b > 0 {
+		return Flag(true)
+	}
+	return Flag(false)
+}
+
+func parseResponse(p []byte) (DNSMessage, error) {
+	res, i := DNSMessage{Data: p}, 0
+	xid, rcode, opcode := binary.BigEndian.Uint16(p[0:2]), p[3]&0x10, (p[2]&0x78)>>3
+	qr, tc, rd, ra, aa := p[2]&0x80, p[2]&0x02, p[2]&0x01, p[3]&0x80, p[2]&0x04
+	i += 4
+
+	var countData struct {
+		Qcount  uint16
+		Ancount uint16
+		Nscount uint16
+		Arcount uint16
+	}
+	r := bytes.NewReader(p[i : i+8])
+	if err := binary.Read(r, binary.BigEndian, &countData); err != nil {
+		fmt.Println("binary.Read err:", err)
+	}
+	i += 8
+
+	res.Header = Header{
+		ID:                    int(xid),
+		RCode:                 (Rcode(rcode)),
+		OpCode:                Opcode(opcode),
+		AuthoritativeAnswer:   toFlag(aa),
+		Truncation:            toFlag(tc),
+		RecursionDesired:      toFlag(rd),
+		RecursionAvailable:    toFlag(ra),
+		QR:                    QR(qr),
+		QuestionCount:         int(countData.Qcount),
+		AnswerCount:           int(countData.Ancount),
+		NameServerCount:       int(countData.Nscount),
+		AdditionalRecordCount: int(countData.Arcount),
+	}
+
+	for qIdx := range countData.Qcount {
+		var qname string
+		qname, i = readName(p, i)
+
+		var qc qtypeAndClass
+		r2 := bytes.NewReader(p[i : i+4])
+		if err := binary.Read(r2, binary.BigEndian, &qc); err != nil {
+			fmt.Println("binary.Read err:", err)
+		}
+
+		q := Question{Name: qname, Qtype: RType(qc.Qtype), Qclass: RClass(qc.Qclass)}
+		res.Questions = append(res.Questions, q)
+		i += 4
+		qIdx++
+	}
+
+	for anIdx := range countData.Ancount {
+		var rr ResourceRecord
+		rr, i = getRR(p, i)
+		res.Answers = append(res.Answers, rr)
+		anIdx++
+	}
+
+	for nsIdx := range countData.Nscount {
+		var rr ResourceRecord
+		rr, i = getRR(p, i)
+		res.Authorities = append(res.Authorities, rr)
+		nsIdx++
+	}
+
+	for arIdx := range countData.Arcount {
+		var rr ResourceRecord
+		rr, i = getRR(p, i)
+		res.Additional = append(res.Additional, rr)
+		arIdx++
+	}
+
+	return res, nil
+}
+
+func printResponse(res DNSMessage, start time.Time, addr syscall.SockaddrInet4, n int) {
+	fmt.Println(";; Got answer")
+	fmt.Printf(";; --HEADER-- opcode: %d, status: %d, id: %d\n", int(res.Header.OpCode), int(res.Header.RCode), res.Header.ID)
+
+	fmt.Printf(";; flags:")
+	if res.Header.QR == QRResponse {
+		fmt.Printf(" qr")
+	}
+	if res.Header.Truncation {
+		fmt.Printf(" tc")
+	}
+	if res.Header.RecursionDesired {
+		fmt.Printf(" rd")
+	}
+	if res.Header.RecursionAvailable {
+		fmt.Printf(" ra")
+	}
+	if res.Header.AuthoritativeAnswer {
+		fmt.Printf(" aa")
+	}
+	fmt.Printf("; ")
+
+	fmt.Printf("QUERY: %d ANSWER: %d AUTHORITY: %d ADDITIONAL: %d\n\n",
+		res.Header.QuestionCount, res.Header.AnswerCount, res.Header.NameServerCount, res.Header.AdditionalRecordCount)
+
+	fmt.Println(";; QUESTION SECTION")
+	for _, q := range res.Questions {
+		fmt.Printf("%6s %13s %6s\n", q.Name, ClassMap[q.Qclass], TypesMap[q.Qtype])
+	}
+
+	for i, a := range res.Answers {
+		if i == 0 {
+			fmt.Println(";; ANSWER")
+		}
+		printRR(a)
+	}
+
+	for i, a := range res.Authorities {
+		if i == 0 {
+			fmt.Println("\n;; AUTHORITY")
+		}
+		printRR(a)
+	}
+
+	for i, a := range res.Authorities {
+		if i == 0 {
+			fmt.Println("\n;; ADDITIONAL")
+		}
+		printRR(a)
+	}
+
+	t := time.Now()
+	elapsed := t.Sub(start)
+	serverIP := netip.AddrFrom4(addr.Addr).String()
+
+	fmt.Println()
+	fmt.Printf(";; Query Time: %d msec\n", elapsed.Milliseconds())
+	fmt.Printf(";; SERVER %s#%d(%s)\n", serverIP, addr.Port, serverIP)
+	fmt.Printf(";; WHEN: %s\n", start.Local().Format("Mon Jan 2 15:04:05 MST 2006"))
+	fmt.Printf(";; MSG SIZE    rcvd: %d\n", n)
+
+}
+
+func getRR(p []byte, i int) (ResourceRecord, int) {
+	recordName, i := readName(p, i)
+	var data struct {
+		RecordType  uint16
+		RecordClass uint16
+		RecordTTL   uint32
+		DataLength  uint16
+	}
+	r := bytes.NewReader(p[i : i+10])
+	if err := binary.Read(r, binary.BigEndian, &data); err != nil {
+		fmt.Println("binary.Read err: ", err)
+	}
+	i += 10
+
+	recordType, dataLength, ttl := RType(data.RecordType), int(data.DataLength), int(data.RecordTTL)
+	recordData, i := formatRData(p, dataLength, recordType, i)
+
+	rr := ResourceRecord{
+		Name:       recordName,
+		Type:       recordType,
+		TTL:        ttl,
+		Class:      RClass(data.RecordClass),
+		DataLength: dataLength,
+		RData:      recordData,
+	}
+
+	return rr, i
+}
+
+func printRR(rr ResourceRecord) {
+	fmt.Printf("%6s %6d %6s %6s    %s\n",
+		rr.Name, rr.TTL, ClassMap[rr.Class], TypesMap[rr.Type], rr.RData)
+}
+
+func formatRData(p []byte, dl int, recordType RType, i int) (string, int) {
+	mailPreference, rdata := 0, ""
+	if dl == 4 && recordType == A {
+		rdata = netip.AddrFrom4([4]byte(p[i : i+4])).String()
+		i += dl
+	}
+
+	if dl == 16 && recordType == AAAA {
+		rdata = netip.AddrFrom16([16]byte(p[i : i+16])).String()
+		i += dl
+	}
+
+	if recordType == TXT {
+		txt := p[i : i+dl]
+		if utf8.Valid(txt) {
+			rdata = string(txt)
+		} else {
+			fmt.Println("invalid txt field")
+			os.Exit(1)
+		}
+		i += dl
+	}
+
+	if recordType == SOA {
+		mname, i := readName(p, i)
+		rname, i := readName(p, i)
+
+		var data struct {
+			Serial  uint32
+			Refresh uint32
+			Retry   uint32
+			Expire  uint32
+		}
+		r := bytes.NewReader(p[i : i+16])
+		if err := binary.Read(r, binary.BigEndian, &data); err != nil {
+			fmt.Println("binary.Read failed:", err)
+		}
+		i += 16
+
+		rdata = fmt.Sprintf("%s %s %d %d %d %d",
+			mname, rname, int(data.Serial), int(data.Refresh), int(data.Retry), int(data.Expire))
+	}
+
+	if recordType == MX {
+		mailPreference = int(binary.BigEndian.Uint16(p[i : i+2]))
+		i += 2
+	}
+
+	if recordType == CNAME || recordType == NS || recordType == MX || recordType == PTR {
+		rdata, i = readName(p, i)
+	}
+
+	if mailPreference != 0 {
+		return fmt.Sprintf("%d %s", mailPreference, rdata), i
+	}
+
+	return rdata, i
+}
+
+func getResourceType(i RType) string {
+	c := TypesMap[i]
+	if c == "" {
+		fmt.Println("invalid type")
+		os.Exit(1)
+	}
+	return c
+}
+
+func getClass(i RClass) string {
+	c := ClassMap[i]
+	if c == "" || i != IN {
+		fmt.Println("invalid class: ", c, i)
+		os.Exit(1)
+	}
+	return c
+}
+
+func typeStringToRtype(s string) RType {
+	for k, v := range TypesMap {
+		if v == s {
+			return k
+		}
+	}
+	log.Fatal("invalid qtype")
+	return 0
+}
