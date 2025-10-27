e1d5a3a22b9c49883a18133c3dd1c324c22dbc57
Jack Fowler <jack@Mikan.local>
2025-10-27 18:17:42 +0900

    add container creation -- primitive

---
+++ os/virtual-machines-containers/chroot/runcontained.c
@@ -0,0 +1,61 @@
+#define _GNU_SOURCE
+#include <sched.h>
+#include <stdio.h>
+#include <stdlib.h>
+#include <sys/wait.h>
+#include <unistd.h>
+
+#define STACK_SIZE 65536
+#define CHROOT_DIR "myroot"
+
+struct child_config {
+  int argc;
+  char **argv;
+};
+
+/* Entry point for child after `clone` */
+int child(void *arg) {
+  if (chroot(CHROOT_DIR) != 0) { 
+    perror("chroot failed");
+    return -1;
+  }
+
+  if (chdir(CHROOT_DIR) != 0) {
+    perror("getcwd failed");
+    return -1;
+  }
+
+  struct child_config *config = arg;
+  if (execvpe(config->argv[0], config->argv, NULL)) {
+    fprintf(stderr, "execvpe failed %m.\n");
+    return -1;
+  }
+  return 0;
+}
+
+int main(int argc, char **argv) {
+  struct child_config config = {0};
+  int flags = 0;
+  pid_t child_pid = 0;
+
+  // Prepare child configuration
+  config.argc = argc - 1;
+  config.argv = &argv[1];
+
+  // Allocate stack for child
+  char *stack = 0;
+  if (!(stack = malloc(STACK_SIZE))) {
+    fprintf(stderr, "Malloc failed");
+    exit(1);
+  }
+
+  // Clone parent, enter child code
+  if ((child_pid = clone(child, stack + STACK_SIZE, flags | SIGCHLD, &config)) == -1) {
+    fprintf(stderr, "Clone failed");
+    exit(2);
+  }
+
+  wait(NULL);
+
+  return 0;
+}
